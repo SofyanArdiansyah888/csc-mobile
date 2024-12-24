e0f0ead7e525f2b62068c824b622f6bcf963f84e
sofyan <sofyanardiansyah888@gmail.com>
2024-12-24 10:41:10 +0800

    fix: change wa url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   baseUrl: `https://crc.banisadar.com/api`,
   imageUrl:`https://crc.banisadar.com/img/`,
-  midtransUrl: `https://crc.banisadar.com/api/get-token`
+  midtransUrl: `https://crc.banisadar.com/api/get-token`,
+  waUrl: `https://wa.me/6285211818886?text=Halo%Karsa`
 };
 
