18332da0782b4e1037a20e22ee042bc08da78829
sofyan <sofyanardiansyah888@gmail.com>
2024-12-28 14:23:53 +0800

    refactor: change model column

--- src/app/entities/Promo.entity.ts
+++ src/app/entities/Promo.entity.ts
@@ -1,6 +1,9 @@
 export interface PromoEntity {
-  name: string;
-  description: string;
-  image: string;
+  nama: string;
+  deskripsi: string;
+  link_gambar: string;
+  tgl_expire_voucher: string;
+  kode_voucher: string;
+  nilai_voucher: number;
 }
 
