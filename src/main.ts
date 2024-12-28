2188953e1827b375d9204daf5bff7b84120cb81f
sofyan <sofyanardiansyah888@gmail.com>
2024-12-28 14:04:37 +0800

    refactor: remove unused import from main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,11 @@ import {IonicRouteStrategy, provideIonicAngular} from '@ionic/angular/standalone
 
 import {routes} from './app/app.routes';
 import {AppComponent} from './app/app.component';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import {providePrimeNG} from 'primeng/config';
-import Aura from '@primeng/themes/aura';
+// import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideAnimations(),
-    providePrimeNG({
-      theme: {
-        preset: Aura
-      }
-    }),
+    // provideAnimations(),
     {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
